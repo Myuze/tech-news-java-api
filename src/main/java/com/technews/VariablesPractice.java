8207c8795e4c905a910aa5042140be8f33438c82
Myuze <Myuze@users.noreply.github.com>
2021-12-26 17:18:39 -0800

    Added Entity Models

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,16 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int integer = 1;
+        double dub = 0.5;
+        double sum = integer + dub;
+
+        String cat = "Shigu";
+
+        String derp = sum + cat;
+
+        System.out.println(sum);
+        System.out.println(derp);
+    }
+}
